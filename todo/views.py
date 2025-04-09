3b2a566e52ab72e50bed7326019a184f9477b13c
Alex <alangsam98@gmail.com>
2025-04-09 14:22:07 -0400

    fix urgency and recurring option in task creation

--- todo/views.py
+++ todo/views.py
@@ -24,3 +24,7 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('task_list')
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'todo/task_detail.html', {'task': task})
