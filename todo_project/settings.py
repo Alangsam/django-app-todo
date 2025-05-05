ad3ab0f0613f57f2217caad440fcbc90e00401cf
Alex <alangsam98@gmail.com>
2025-05-04 21:42:16 -0400

    .

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'db',  # or 'db' if using Docker
+        'PORT': '8000',
     }
 }
+
 
 
 # Password validation
